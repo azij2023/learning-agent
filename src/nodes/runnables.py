f9151d8a46d1577030d176488426590b60c0bde6
Azij <azij2023@gmail.com>
2026-02-09 19:20:35 +0530

    Update root rnnables

--- src/nodes/runnables.py
+++ src/nodes/runnables.py
@@ -4,23 +4,22 @@ from src.nodes.define_checkpoint import define_checkpoint
 from src.nodes.gather_context import gather_context
 from src.nodes.validate_context import validate_context
 from src.nodes.context_processor import context_processor
-from src.nodes.topic_explainer import topic_explainer   # NEW
+from src.nodes.topic_explainer import topic_explainer
 from src.nodes.question_generator import question_generator
 from src.nodes.verifier import verifier
 from src.nodes.logic import conditional_logic
+from src.nodes.feynman import feynman_node
 
 # Wrap each node
 define = RunnableLambda(define_checkpoint)
 gather = RunnableLambda(gather_context)
 validate = RunnableLambda(validate_context)
 process = RunnableLambda(context_processor)
-explain = RunnableLambda(topic_explainer)              # NEW
+explain = RunnableLambda(topic_explainer)
 generate = RunnableLambda(question_generator)
-verify = RunnableLambda(lambda state: verifier(state, [
-    "Probability is the measure of likelihood of events.",
-    "Events can be independent or mutually exclusive in probability theory."
-]))
+verify = RunnableLambda(verifier)
 logic = RunnableLambda(conditional_logic)
+feynman = RunnableLambda(feynman_node)
 
 # Sequence the pipeline
 agent_pipeline = (
@@ -28,8 +27,9 @@ agent_pipeline = (
     .pipe(gather)
     .pipe(validate)
     .pipe(process)
-    .pipe(explain)     # teach first
-    .pipe(generate)    # then test
-    .pipe(verify) 
-    .pipe(logic) 
+    .pipe(explain)
+    .pipe(generate)
+    .pipe(verify)
+    .pipe(logic)
+    .pipe(feynman)
 )
